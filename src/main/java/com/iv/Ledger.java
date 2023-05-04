e2271fa6b1a2496a1fc8b169c6f4e5b44d440160
isaiah vang <vang.isaiah3@gmail.com>
2023-05-04 12:53:55 -0400

    Final commit, complete product

--- src/main/java/com/iv/Ledger.java
+++ src/main/java/com/iv/Ledger.java
@@ -68,35 +68,53 @@ public class Ledger {
     }
 
     public void addDeposit() {
-        System.out.println("Please provide the date of your deposit(yyyy-MM-dd): \n Answer: ");
-        String date = scanner.nextLine();
-
-        System.out.println("Please provide the time of your deposit(hh:MM:ss): \n Answer: ");
-        String time = scanner.nextLine();
-
-        System.out.println("Please provide a description: \n Answer: ");
-        String desc = scanner.nextLine();
-
-        System.out.println("Please provide the vendor: \n Answer: ");
-        String vendor = scanner.nextLine();
-
-        System.out.println("Please provide the amount you are intending to deposit: \n Answer: ");
-        String amount = scanner.nextLine();
         try {
-            FileWriter depositFiles = new FileWriter("./src/main/java/com/iv/Transactions.txt", true);
-            BufferedWriter bufferedWriter = new BufferedWriter(depositFiles);
+            System.out.println("Please provide the date of your deposit(yyyy-MM-dd): \n Answer: ");
+            String date = scanner.nextLine();
 
-            bufferedWriter.write("\n" + date + "|" + time + "|" + desc + "|" + vendor + "|" + amount);
+            System.out.println("Please provide the time of your deposit(hh:MM:ss): \n Answer: ");
+            String time = scanner.nextLine();
 
-            System.out.println("Deposit successful!");
-            bufferedWriter.close();
-        } catch (Exception e) {
-            System.out.println("Deposit unsuccessful, error code 1053R");
+            System.out.println("Please provide a description: \n Answer: ");
+            String desc = scanner.nextLine();
+
+            System.out.println("Please provide the vendor: \n Answer: ");
+            String vendor = scanner.nextLine();
+
+            System.out.println("Please provide the amount you are intending to deposit: \n Answer: ");
+            float amount = scanner.nextFloat();
+            try {
+                FileWriter depositFiles = new FileWriter("./src/main/java/com/iv/Transactions.txt", true);
+                BufferedWriter bufferedWriter = new BufferedWriter(depositFiles);
+
+                bufferedWriter.write("\n" + date + "|" + time + "|" + desc + "|" + vendor + "|" + amount);
+
+                System.out.println("Deposit successful!");
+                bufferedWriter.close();
+            } catch (Exception e) {
+                System.out.println("Deposit unsuccessful, error code 1053R");
+                e.printStackTrace();
+            }
+        }catch (Exception e){
+            System.out.println("There was an incorrect input, please try again.");
             e.printStackTrace();
         }
+//        try {
+//            FileWriter depositFiles = new FileWriter("./src/main/java/com/iv/Transactions.txt", true);
+//            BufferedWriter bufferedWriter = new BufferedWriter(depositFiles);
+//
+//            bufferedWriter.write("\n" + date + "|" + time + "|" + desc + "|" + vendor + "|" + amount);
+//
+//            System.out.println("Deposit successful!");
+//            bufferedWriter.close();
+//        } catch (Exception e) {
+//            System.out.println("Deposit unsuccessful, error code 1053R");
+//            e.printStackTrace();
+//        }
     }
 
     public void makePayment() {
+        try {
             System.out.println("Please provide the date of your payment(yyyy-MM-DD): \n Answer: ");
             String date = scanner.nextLine();
 
@@ -111,18 +129,34 @@ public class Ledger {
 
             System.out.println("Please provide the amount you are intending to pay: \n Answer: ");
             Float amount = scanner.nextFloat();
-        try {
-            FileWriter depositFiles = new FileWriter("./src/main/java/com/iv/Transactions.txt", true);
-            BufferedWriter bufferedWriter = new BufferedWriter(depositFiles);
+            try {
+                FileWriter depositFiles = new FileWriter("./src/main/java/com/iv/Transactions.txt", true);
+                BufferedWriter bufferedWriter = new BufferedWriter(depositFiles);
 
-            bufferedWriter.write("\n" + date + "|" + time + "|" + desc + "|" + vendor + "|" + "-" + amount);
+                bufferedWriter.write("\n" + date + "|" + time + "|" + desc + "|" + vendor + "|" + "-" + amount);
 
-            System.out.println("Payment successful!");
-            bufferedWriter.close();
-        } catch (IOException e) {
-            System.out.println("Payment unsuccessful, error code 1053R");
+                System.out.println("Payment successful!");
+                bufferedWriter.close();
+            } catch (Exception e) {
+                System.out.println("Payment unsuccessful, error code 1053R");
+                e.printStackTrace();
+            }
+        } catch (Exception e){
+            System.out.println("There was an incorrect input, please try again.");
             e.printStackTrace();
         }
+//        try {
+//            FileWriter depositFiles = new FileWriter("./src/main/java/com/iv/Transactions.txt", true);
+//            BufferedWriter bufferedWriter = new BufferedWriter(depositFiles);
+//
+//            bufferedWriter.write("\n" + date + "|" + time + "|" + desc + "|" + vendor + "|" + "-" + amount);
+//
+//            System.out.println("Payment successful!");
+//            bufferedWriter.close();
+//        } catch (Exception e) {
+//            System.out.println("Payment unsuccessful, error code 1053R");
+//            e.printStackTrace();
+//        }
     }
 
     public void ledgerMenu() {
